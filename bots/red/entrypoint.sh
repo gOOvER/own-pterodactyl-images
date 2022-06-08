cc9e14b498c6378a98a9c65d6a50aac29e24c8de
gOOvER <info@goover.de>
2022-06-08 09:57:02 +0200

    pip update

--- bots/red/entrypoint.sh
+++ bots/red/entrypoint.sh
@@ -5,6 +5,8 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+/usr/local/bin/python -m pip install --upgrade pip
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
