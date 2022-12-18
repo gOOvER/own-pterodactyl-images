bb6406468e6e6e06718ba04062bbe8967c9ca3f0
gOOvER <info@goover.de>
2022-12-18 10:49:54 +0100

    some fixes

--- bots/bastion/entrypoint.sh
+++ bots/bastion/entrypoint.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#Variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+#show versions
+echo -e "${BLUE}-------------------------------------------------${NC}"
+echo -e "${YELLOW}BastionBot Installation${NC}"
+echo -e "${BLUE}-------------------------------------------------${NC}"
+echo -e "${YELLOW}MongoDB Version:${NC}"; mongod --version
+echo -e "${YELLOW}NodeJS Version:${NC}"; node -v
+echo -e "${YELLOW}NPM Version:${NC}"; npm -v
+echo -e "${YELLOW}Python Version:${NC}"; python -version
+echo -e "${BLUE}-------------------------------------------------${NC}"
+
 cd /home/container
 
 # Set environment variable that holds the Internal Docker IP
@@ -10,7 +27,7 @@ MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # start mongo
-/usr/bin/mongod --fork --dbpath /home/container/mongodb/ --port 27017 --logpath /home/container/mongod.log && until nc -z -v -w5 127.0.0.1 27017; do echo 'Waiting for mongodb connection...'; sleep 5; done
+mongod --fork --dbpath /home/container/mongodb/ --port 27017 --logpath /home/container/mongod.log --logRotate reopen --logappend && until nc -z -v -w5 127.0.0.1 27017; do echo 'Waiting for mongodb connection...'; sleep 5; done
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
