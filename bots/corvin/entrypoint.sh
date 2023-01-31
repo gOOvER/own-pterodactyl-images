a092d9d91b1aa722006daa5e2c62a72f3815acb1
gOOvER <info@goover.de>
2023-01-31 13:39:03 +0100

    fix

--- bots/corvin/entrypoint.sh
+++ bots/corvin/entrypoint.sh
@@ -16,8 +16,8 @@ echo -e "${YELLOW}MongoDB Version:${NC} " && mongod --version
 echo -e "${YELLOW}NodeJS Version:${NC} " && node -v
 echo -e "${YELLOW}NPM Version:${NC} " && npm -v
 echo -e "${YELLOW}Java Version:${NC} " && java -version
-echo -e "${YELLOW}Running on Debian:${NC} " cat /etc/debian_version
-echo -e "${YELLOW}Current timezone:${NC} " cat /etc/timezone
+echo -e "${YELLOW}Running on Debian:${NC} " && cat /etc/debian_version
+echo -e "${YELLOW}Current timezone:${NC} " && cat /etc/timezone
 echo -e "${BLUE}-------------------------------------------------${NC}"
 
 cd /home/container
@@ -43,4 +43,4 @@ echo -e "${BLUE}-------------------------------------------------${NC}"
 eval ${MODIFIED_STARTUP}
 
 # stop mongo
-mongo --eval "db.getSiblingDB('admin').shutdownServer()"
+mongod --eval "db.getSiblingDB('admin').shutdownServer()"
