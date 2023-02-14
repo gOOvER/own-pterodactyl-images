e4b7d98914e891806ebdd4c1406d6b20046dd46e
gOOvER <info@goover.de>
2023-02-14 15:20:40 +0100

    update proton

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -11,6 +11,14 @@ export TZ
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+# proton envs
+if [ -f "/usr/local/bin/proton" ]; then
+    echo -e "Proton found. Setting needed envs..."
+    mkdir -p /mnt/server/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
+    export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
+    export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}"
+fi
+
 # Information output
 echo "Running on: $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
