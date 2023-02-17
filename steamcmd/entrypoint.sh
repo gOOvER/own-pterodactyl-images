24ab479a54d09655e796995e5e32c0b85dfa6ebd
gOOvER <info@goover.de>
2023-02-17 15:01:08 +0100

    update paths for protontricks

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -39,6 +39,10 @@ if [ -f "/usr/local/bin/proton" ]; then
 	    mkdir -p /home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
         export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
         export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}"
+        #protontricks
+        export STEAM_DIR="/home/container/.steam/steam"
+        export WINETRICKS="/usr/sbin/winetricks"
+
     else
         echo -e "----------------------------------------------------------------------------------"
         echo -e "WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it"
