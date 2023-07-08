9ad58a4a431dfa6a1792af787a6dd0cf1f949e3e
gOOvER <116325+gOOvER@users.noreply.github.com>
2023-07-08 13:27:48 +0200

    image updates part 6

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+clear
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 # Wait for the container to fully initialize
 sleep 1
 
@@ -10,6 +17,12 @@ export TZ
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
+
+# Information output
+echo -e "${BLUE}-------------------------------------------------${NC}"
+echo -e "${YELLOW}Running on Debian $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}Current timezone: $(cat /etc/timezone)${NC}"
+echo -e "${BLUE}-------------------------------------------------${NC}"
 
 # Set environment for Steam Proton
 if [ -f "/usr/local/bin/proton" ]; then
@@ -23,10 +36,10 @@ if [ -f "/usr/local/bin/proton" ]; then
         #export STEAM_RUNTIME=1
 
     else
-        echo -e "----------------------------------------------------------------------------------"
-        echo -e "WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it"
-        echo -e "Server stops now"
-        echo -e "----------------------------------------------------------------------------------"
+        echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
+        echo -e "${RED}WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it${NC}"
+        echo -e "${RED}Server stops now${NC}"
+        echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
         exit 0
         fi
 fi
@@ -36,13 +49,17 @@ cd /home/container || exit 1
 
 ## just in case someone removed the defaults.
 if [ "${STEAM_USER}" == "" ]; then
-    echo -e "steam user is not set.\n"
-    echo -e "Using anonymous user.\n"
+    echo -e "${BLUE}-------------------------------------------------${NC}"
+    echo -e "${YELLOW}Steam user is not set. ${NC}"
+    echo -e "${YELLOW}Using anonymous user.${NC}"
+    echo -e "${BLUE}-------------------------------------------------${NC}"
     STEAM_USER=anonymous
     STEAM_PASS=""
     STEAM_AUTH=""
 else
-    echo -e "user set to ${STEAM_USER}"
+    echo -e "${BLUE}-------------------------------------------------${NC}"
+    echo -e "${YELLOW}user set to ${STEAM_USER} ${NC}"
+    echo -e "${BLUE}-------------------------------------------------${NC}"
 fi
 
 ## if auto_update is not set or to 1 update
@@ -55,11 +72,15 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
             numactl --physcpubind=+0 ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
 	    fi
     else
-        echo -e "No appid set. Starting Server"
+        echo -e "${BLUE}-------------------------------------------------${NC}"
+        echo -e "${YELLOW}No appid set. Starting Server${NC}"
+        echo -e "${BLUE}-------------------------------------------------${NC}"
     fi
 
 else
-    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+    echo -e "${BLUE}---------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}Not updating game server as auto update was set to 0. Starting Server${NC}"
+    echo -e "${BLUE}---------------------------------------------------------------${NC}"
 fi
 
 # Replace Startup Variables
