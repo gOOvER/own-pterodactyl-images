d5cb2be702355ead236f62c844a81e383213d05a
gOOvER <116325+gOOvER@users.noreply.github.com>
2023-10-26 13:08:52 +0200

    missed mono version

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -25,7 +25,6 @@ echo -e "${BLUE}-------------------------------------------------${NC}"
 echo -e "${YELLOW}Running on Debian: ${RED} $(cat /etc/debian_version)${NC}"
 echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
 echo -e "${YELLOW}DotNet Version: ${RED} $(dotnet --version) ${NC}"
-echo -e "${YELLOW}Mono Version: ${RED} $(mono --version) ${NC}"
 echo -e "${BLUE}-------------------------------------------------${NC}"
 
 # Set environment for Steam Proton
