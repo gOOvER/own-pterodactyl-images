ec9b05eb143816f3d24ec6e59d36627dcbf5832b
gOOvER <info@goover.de>
2023-12-18 15:59:08 +0100

    cleanup entrypoint

--- games/wurm/entrypoint.sh
+++ games/wurm/entrypoint.sh
@@ -73,57 +73,9 @@ if [[ $XVFB == 1 ]]; then
         Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
 fi
 
-# Install necessary to run packages
-echo -e "${BLUE}-------------------------------------------------${NC}"
-echo -e "${RED}First launch will throw some errors. Ignore them${NC}"
-echo -e "${BLUE}-------------------------------------------------${NC}"
-mkdir -p $WINEPREFIX
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Check if wine-gecko required and install it if so
-if [[ $WINETRICKS_RUN =~ gecko ]]; then
-        echo -e "${BLUE}-------------------------------------------------${NC}"
-        echo -e "${YELLOW}Installing Wine Gecko${NC}"
-        echo -e "${BLUE}-------------------------------------------------${NC}"
-        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
-
-        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
-        fi
-
-        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
-        fi
-
-        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
-        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
-fi
-
-# Check if wine-mono required and install it if so
-if [[ $WINETRICKS_RUN =~ mono ]]; then
-        echo -e "${BLUE}-------------------------------------------------${NC}"
-        echo -e "${YELLOW}Installing Wine Mono${NC}"
-        echo -e "${BLUE}-------------------------------------------------${NC}"
-        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
-
-        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
-                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
-        fi
-
-        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
-fi
-
-# List and install other packages
-for trick in $WINETRICKS_RUN; do
-        echo -e "${BLUE}-------------------------------------------------${NC}"
-        echo -e "${YELLOW}Installing: ${NC} ${GREEN} $trick ${NC}"
-        echo -e "${BLUE}-------------------------------------------------${NC}"
-        winetricks -q $trick
-done
-
-# Replace variables in the startup command
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-printf "%s%s\n" "$IMAGE_PROMPT" "$PARSED"
-
-# Run the startup command
-# shellcheck disable=SC2086
-exec env ${PARSED}
+# Run the Server
+eval ${MODIFIED_STARTUP}
