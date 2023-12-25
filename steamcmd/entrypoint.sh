47bfeaed992c240312b39580fb87abfee64cd448
gOOvER <info@goover.de>
2023-12-25 12:27:55 +0100

    Update entrypoint.sh

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -19,39 +19,13 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Information output
-if [ -f "/usr/local/bin/proton" ]; then
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-    echo -e "${RED}SteamCMD Proton  Image by gOOvER${NC}"
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-    echo -e "${YELLOW}Running on Debian: ${RED} $(cat /etc/debian_version)${NC}"
-    echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-else
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-    echo -e "${RED}SteamCMD Image by gOOvER${NC}"
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-    echo -e "${YELLOW}Running on Debian: ${RED} $(cat /etc/debian_version)${NC}"
-    echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
-    echo -e "${YELLOW}DotNet Version: ${RED} $(dotnet --version) ${NC}"
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-fi
-
-# Set environment for Steam Proton
-if [ -f "/usr/local/bin/proton" ]; then
-    if [ ! -z ${SRCDS_APPID} ]; then
-	    mkdir -p /home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
-        export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
-        export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}"
-        export WINETRICKS="/usr/sbin/winetricks"
-
-    else
-        echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-        echo -e "${RED}WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it${NC}"
-        echo -e "${RED}Server stops now${NC}"
-        echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
-        exit 0
-        fi
-fi
+echo -e "${BLUE}-------------------------------------------------${NC}"
+echo -e "${RED}SteamCMD Image by gOOvER${NC}"
+echo -e "${BLUE}-------------------------------------------------${NC}"
+echo -e "${YELLOW}Running on Debian: ${RED} $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
+echo -e "${YELLOW}DotNet Version: ${RED} $(dotnet --version) ${NC}"
+echo -e "${BLUE}-------------------------------------------------${NC}"
 
 # Switch to the container's working directory
 cd /home/container || exit 1
@@ -84,7 +58,7 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     # Update Source Server
     if [ ! -z ${SRCDS_APPID} ]; then
 	    if [ "${STEAM_USER}" == "anonymous" ]; then
-            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) $( [[ "${STEAM_SDK}" == "1" ]] && printf %s '+app_update 1007' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
 	    else
             numactl --physcpubind=+0 ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
 	    fi
