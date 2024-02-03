0ef47bc22e4797075ee9e6cc89af3778d8a9da01
gOOvER <info@goover.de>
2024-02-03 08:17:44 +0100

    add experimental protontricks

--- steam/entrypoint.sh
+++ steam/entrypoint.sh
@@ -93,6 +93,14 @@ echo -e "${BLUE}----------------------------------------------------------------
 echo -e "${GREEN}Starting Server.... Please wait...${NC}"
 echo -e "${BLUE}----------------------------------------------------------------------------------${NC}"
 
+# List and install other packages
+for trick in $PROTONTRICKS_RUN; do
+        echo -e "${BLUE}-------------------------------------------------${NC}"
+        echo -e "${YELLOW}Installing: ${NC} ${GREEN} $trick ${NC}"
+        echo -e "${BLUE}-------------------------------------------------${NC}"
+        protontricks ${SRCDS_APPID} $trick
+done
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
