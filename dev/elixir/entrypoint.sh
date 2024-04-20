f1686d561739651f4ac00a54e5838395fb0af91c
gOOvER <info@goover.de>
2024-04-20 14:29:01 +0200

    add elixir images

---
+++ dev/elixir/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#System variables
+clear
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Wait for the container to fully initialize
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# system informations
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${RED}BUN Image by gOOvER${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}Running on Debian: ${RED} $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+echo -e "${YELLOW}Elixir Version: ${RED} $(elixir -e 'IO.puts "Elixir " <> System.version()') ${NC}"
+echo -e "${BLUE}---------------------------------------------------------------------${NC}"
+
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
