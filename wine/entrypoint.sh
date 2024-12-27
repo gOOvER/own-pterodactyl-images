478eab819f685a46798eba40d38e5f2852e3140f
gOOvER <116325+gOOvER@users.noreply.github.com>
2024-12-27 12:37:31 +0100

    Update entrypoint.sh

--- wine/entrypoint.sh
+++ wine/entrypoint.sh
@@ -53,7 +53,7 @@ fi
 ## if auto_update is not set or to 1 update
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
 	if [ -f /home/container/DepotDownloader ]; then
-		./DepotDownloader -dir /home/container $( [[ -z ${STEAM_USER} ]] || printf %s "-username ${STEAM_USER} -password ${STEAM_PASS} -remember-password" ) $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '-os windows' ) -app ${STEAM_APPID} $( [[ -z ${STEAM_BETAID} ]] || printf %s "-beta ${STEAM_BETAID}" ) $( [[ -z ${STEAM_BETAPASS} ]] || printf %s "-betapassword ${STEAM_BETAPASS}" ) -validate
+		./DepotDownloader -dir /home/container -username ${STEAM_USER} -password ${STEAM_PASS} -remember-password $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '-os windows' ) -app ${STEAM_APPID} $( [[ -z ${STEAM_BETAID} ]] || printf %s "-beta ${STEAM_BETAID}" ) $( [[ -z ${STEAM_BETAPASS} ]] || printf %s "-betapassword ${STEAM_BETAPASS}" )
 		mkdir -p /home/container/.steam/sdk64
 		./DepotDownloader -dir /home/container/.steam/sdk64 -app 1007
 		chmod +x $HOME/*
