fa489a4bb80fd5a318035d8c1da356ccdcd90443
gOOvER <116325+gOOvER@users.noreply.github.com>
2024-12-27 12:30:18 +0100

    typo

--- wine/entrypoint.sh
+++ wine/entrypoint.sh
@@ -54,8 +54,8 @@ fi
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
 	if [ -f /home/container/DepotDownloader ]; then
 		./DepotDownloader -dir /home/container $( [[ -z ${STEAM_USER} ]] || printf %s "-username ${STEAM_USER} -password ${STEAM_PASS} -remember-password" ) $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '-os windows' ) -app ${STEAM_APPID} $( [[ -z ${STEAM_BETAID} ]] || printf %s "-beta ${STEAM_BETAID}" ) $( [[ -z ${STEAM_BETAPASS} ]] || printf %s "-betapassword ${STEAM_BETAPASS}" ) -validate
-		mkdir -p /mnt/server/.steam/sdk64
-		./DepotDownloader -dir /mnt/server/.steam/sdk64 -app 1007
+		mkdir -p /home/container/.steam/sdk64
+		./DepotDownloader -dir /home/container/.steam/sdk64 -app 1007
 		chmod +x $HOME/*
 
 	else
