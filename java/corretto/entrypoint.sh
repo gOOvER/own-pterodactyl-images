242e7152a5076e1cf7e5c2bc4df44ce608391e6e
gOOvER <116325+gOOvER@users.noreply.github.com>
2025-01-12 12:04:09 +0100

    add more License infos

--- java/corretto/entrypoint.sh
+++ java/corretto/entrypoint.sh
@@ -25,6 +25,7 @@ export INTERNAL_IP
 # system informations
 echo -e "${BLUE}---------------------------------------------------------------------${NC}"
 echo -e "${RED}Java Corretto Image by gOOvER - https://discord.goover.dev${NC}"
+echo -e "${RED}THIS IMAGE IS LICENSED UNDER AGPLv3${NC}"
 echo -e "${BLUE}---------------------------------------------------------------------${NC}"
 echo -e "${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}"
 echo -e "${YELLOW}Current timezone: ${RED} $(cat /etc/timezone)${NC}"
