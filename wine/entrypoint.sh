524ea2065557041b13544ab2c6dec667b5e5a249
gOOvER <116325+gOOvER@users.noreply.github.com>
2025-01-16 07:40:52 +0100

    typo

--- wine/entrypoint.sh
+++ wine/entrypoint.sh
@@ -20,7 +20,7 @@ echo -e "${BLUE}----------------------------------------------------------------
 echo -e "${YELLOW}Wine Image from gOOvER${NC}"
 echo -e "${RED}THIS IMAGE IS LICENSED UNDER AGPLv3${NC}"
 echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-echo -e "${YELLOW}echo -e "${YELLOW}Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}" $(cat /etc/debian_version)${NC}"
+echo -e "${YELLOW}echo -e "${YELLOW}Docker Linux Distribution: ${RED} $(. /etc/os-release ; echo $PRETTY_NAME)${NC}" $(cat /etc/debian_version)${NC}"
 echo -e "${YELLOW}Current timezone: $(cat /etc/timezone)${NC}"
 echo -e "${YELLOW}Wine Version:${NC} ${RED} $(wine --version)${NC}"
 echo -e "${BLUE}---------------------------------------------------------------------${NC}"
